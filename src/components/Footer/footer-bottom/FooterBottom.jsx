3176c7798add51aef5a5da34f5bc3c70f15accb4
Ebubekir Rüzğar <ebubekirrzgr@gmail.com>
2022-01-04 02:05:02 +0300

    footer-bottom added

--- src/components/Footer/footer-bottom/FooterBottom.jsx
+++ src/components/Footer/footer-bottom/FooterBottom.jsx
@@ -2,10 +2,37 @@ import './footer-bottom.scss';
 
 import React from 'react';
 
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import facebook from '../../../assets/png/fb.png';
+import twitter from '../../../assets/png/twit.png';
+import instagram from '../../../assets/png/ig.png';
+
 function FooterBottom() {
   return (
     <div>
-      <p>sa</p>
+      <Navbar className="footer-bottom" bg="light" variant="light">
+        <Container>
+          <Nav className="me-auto social-medias">
+            <Nav.Link href="#facebook">
+              <img src={facebook} alt="facebook" />
+            </Nav.Link>
+            <Nav.Link href="#twitter">
+              <img src={twitter} alt="twitter" />
+            </Nav.Link>
+            <Nav.Link href="#instagram">
+              <img src={instagram} alt="instagram" />
+            </Nav.Link>
+          </Nav>
+          <Nav className="me-auto copyright">
+            <p>Copyright @ 2020 Themesmile</p>
+          </Nav>
+          <Nav className="me-auto terms">
+            <Nav.Link href="#home">Legals</Nav.Link>
+            <Nav.Link href="#services">Term & Conditions</Nav.Link>
+            <Nav.Link href="#downloads">License</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </div>
   );
 }
