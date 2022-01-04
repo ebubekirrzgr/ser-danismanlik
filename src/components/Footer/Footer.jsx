906f665600a6d2feaebca7cb4d847a164f4e3362
Ebubekir Rüzğar <ebubekirrzgr@gmail.com>
2022-01-04 03:23:59 +0300

    footer-top and footer-bottom combined

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,13 @@
 import './footer.scss';
 
 import React from 'react';
+import FooterTop from './footer-top';
 import FooterBottom from './footer-bottom';
 
 function Footer() {
   return (
     <div>
+      <FooterTop />
       <FooterBottom />
     </div>
   );
