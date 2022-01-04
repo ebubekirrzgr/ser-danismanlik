f0a8afbe56cf1dfdee78ffefcf80e13d101f85bc
Ebubekir Rüzğar <ebubekirrzgr@gmail.com>
2022-01-04 03:23:14 +0300

    footer-bottom bugs fixed

--- src/components/Footer/footer-bottom/FooterBottom.jsx
+++ src/components/Footer/footer-bottom/FooterBottom.jsx
@@ -10,6 +10,7 @@ import instagram from '../../../assets/png/ig.png';
 function FooterBottom() {
   return (
     <div>
+      <div className="footer-line"></div>
       <Navbar className="footer-bottom" bg="light" variant="light">
         <Container>
           <Nav className="me-auto social-medias">
