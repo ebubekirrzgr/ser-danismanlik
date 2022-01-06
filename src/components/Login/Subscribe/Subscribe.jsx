d7d179832d982c446796676a0db5f9d1cce1118f
Ebubekir Rüzğar <ebubekirrzgr@gmail.com>
2022-01-06 23:07:09 +0300

    subscribe component added

---
+++ src/components/Login/Subscribe/Subscribe.jsx
@@ -0,0 +1,21 @@
+import './subscribe.scss';
+
+import React from 'react';
+
+function Subscribe() {
+  return (
+    <div className="subscribeContainer">
+      <div className="subscribeWrapper">
+        <p className="subscribeP">Short headline</p>
+        <h2 className="subscribeH2">Subscribe our newsletter</h2>
+        <p className="subscribeText">
+          Online businesses utilize newsletters to keep their brand top-of-mind
+          for consumers, establishing authority.
+        </p>
+        <button className="btnLearn">Learn More</button>
+      </div>
+    </div>
+  );
+}
+
+export default Subscribe;
